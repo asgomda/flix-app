f1f466d05f9a5132cb9ae32aa42d129b590864e4
asgomda <72233905+asgomda@users.noreply.github.com>
2021-09-18 17:13:15 +0000

    Add files via upload

---
+++ flixter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixter
+//
+//  Created by Gomda Abdul samad on 18/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
